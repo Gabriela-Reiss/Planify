bd76ac950d7208ada44f585c81b0f19674f2c394
RaphaelKim21 <koreakim2107@gmail.com>
2025-09-19 11:56:40 -0300

    add theme context and button for theme toggling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,14 @@
 import { Stack } from "expo-router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../src/configurations/i18nConfig";
+import { ThemeProvider } from "../src/context/ContextTheme";
 
 export default function Layout() {
     return (
         <I18nextProvider i18n={i18n}>
+            <ThemeProvider>
             <Stack screenOptions={{ headerShown: false }} />
+            </ThemeProvider>
         </I18nextProvider>
     );
 }
